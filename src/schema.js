038f4689d27be7ad300381d20fbf6e6ce026b664
NosPS <99005010+NosPS@users.noreply.github.com>
2022-07-08 18:14:05 +0700

    Update schema.js

--- src/schema.js
+++ src/schema.js
@@ -3,7 +3,7 @@ const { buildSchema } = require("graphql");
 const schema = buildSchema(`
 type Query {
     nfts: [NFT!]!
-    findNFT(tokenId: ID!): [NFT]!
+    findNFT(tokenId: ID!): NFT!
     logs: [LOG!]!
     findLOG(tokenId: ID!): [LOG!]!
 }
@@ -28,10 +28,10 @@ type LOG {
 
 type Mutation {
     addNFT(tokenId: ID!, owner: String!, uri: String!, price: String!, isListing: Boolean!): [NFT!]!
-    updateNFT(tokenId: ID!, owner: String, uri: String, price: String, isListing: Boolean): [NFT]!
+    updateNFT(tokenId: ID!, owner: String, uri: String, price: String, isListing: Boolean): NFT!
     deleteNFT(tokenId: ID!): NFT!
     addLOG(blockNumber: Int!, transactionHash: String!, tokenId: ID!, from: String!, to: String!, price: String!, timestamp: Int!): [LOG!]!
-    updateLOG(blockNumber: Int, transactionHash: String!, tokenId: ID, from: String, to: String, price: String, timestamp: Int): [LOG]!
+    updateLOG(blockNumber: Int, transactionHash: String!, tokenId: ID, from: String, to: String, price: String, timestamp: Int): LOG!
     deleteLOG(transactionHash: String!): LOG!
 }
 `);
